58783d125e97f46cd73dc2e829cb34136449dd4b
DESPIKABLEEE <grafikaking.gabriel@gmail.com>
2025-11-04 11:26:32 +0100

    napravljen navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,42 @@
 import './Navbar.scss'
-import { MagnifyingGlass, User } from '@phosphor-icons/react'
+import { MagnifyingGlass, House, SpotifyLogo, DownloadSimple  } from '@phosphor-icons/react'
 
 function Navbar() {
   return (
     <>
       <div className="navbar">
         <div className="navbar-left">
-            <div className="search-container">
-                <MagnifyingGlass size={20} />
-                <input type="text" placeholder="Search.."></input>
+            
+            <div className="left-left">
+                <SpotifyLogo size={34} weight='fill' color='white'/>
             </div>
+            <div className="left-right">
+                <div className="home-icon">
+                    <House size={24} weight='fill' color='white'/>
+                </div>
+                <div className="search-container">
+                    {/* <MagnifyingGlass size={34} /> */}
+                    <input type="text" placeholder="Search.."></input>
+                </div>
+            </div>
+            
         </div>
         <div className="navbar-right">
-
+            <div className="right-left">
+            <ul>
+                <li>Premium</li>
+                <li>Support</li>
+                <li>Download</li>
+            </ul>
+            </div>
+            <div className="right-right">
+                <ul>
+                    <li><DownloadSimple size={20} weight='fill' color='white'/>Install App</li>
+                    <li className="signup">Sign up</li>
+                    <li>Log in</li>
+                </ul>
+            </div>
+            
         </div>
       </div>
     </>
